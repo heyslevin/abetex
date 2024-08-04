7cf236daf887cf20a0bbc1533f14c8a6938a9d1a
memellaguno <memellaguno@gmail.com>
2024-08-03 23:45:40 -0600

    Added Carousel correctly

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,12 @@
 import { client } from "../sanity/lib/client";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselPrevious,
+  CarouselNext,
+} from "@/components/ui/carousel";
+import { Card, CardContent } from "@/components/ui/card";
 
 const DATA_QUERY = `*[_type == "page" && title == "Home"]{heading, caption}[0]`;
 
@@ -62,6 +70,28 @@ export default async function Home() {
           </div>
         </div>
         <div className="animate-fade-up my-10 grid w-full max-w-screen-xl grid-cols-1 gap-5 px-5 md:grid-cols-3 xl:px-0"></div>
+        <Carousel className="w-full max-w-sm">
+          <CarouselContent className="-ml-1">
+            {Array.from({ length: 5 }).map((_, index) => (
+              <CarouselItem
+                key={index}
+                className="pl-1 md:basis-1/2 lg:basis-1/3"
+              >
+                <div className="p-1">
+                  <Card>
+                    <CardContent className="flex aspect-square items-center justify-center p-6">
+                      <span className="text-2xl font-semibold">
+                        {index + 1}
+                      </span>
+                    </CardContent>
+                  </Card>
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
       </div>
     </>
   );
