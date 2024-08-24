eb622beb7e27bf24d1c9d44c7810b9e9856b058d
memellaguno <memellaguno@gmail.com>
2024-08-24 09:02:36 -0600

    Duospress v1.0 is ready

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -1,35 +1,35 @@
 import { Separator } from "../ui/separator";
 
-export default function Footer() {
-  const data = {
-    title: "Beautiful Website Corporation",
-    socials: {
-      ig: "#",
-      x: "#",
-      li: "#",
-    },
-    address: "Sydney (Warrang) The Commons 388 George Street Sydney NSW 2000",
-    privacyPolicy: {
-      fileUrl: "#",
-    },
-    legal: {
-      fileUrl: "#",
-    },
-  };
+export default function Footer({ data }) {
+  // const data = {
+  //   title: "Beautiful Website Corporation",
+  //   socials: {
+  //     ig: "#",
+  //     x: "#",
+  //     li: "#",
+  //   },
+  //   address: "Sydney (Warrang) The Commons 388 George Street Sydney NSW 2000",
+  //   privacyPolicy: {
+  //     fileUrl: "#",
+  //   },
+  //   legal: {
+  //     fileUrl: "#",
+  //   },
+  // };
 
   return (
     <div className="flex h-auto flex-col bg-slate-500 px-4 py-10 md:h-[555px] md:px-28">
       <div className="flex w-full flex-col gap-6 md:flex-row">
         <div className="flex w-1/2 flex-col">
-          <p className="text-xl md:text-3xl">{data.title}</p>
+          <p className="text-xl md:text-3xl">{data.websiteTitle}</p>
         </div>
         <div className="flex flex-col md:w-1/2 md:flex-row">
           <div className="flex w-1/2 flex-row">
             <div className="flex w-1/2 flex-col md:w-1/2">
-              <a href={data?.socials.ig} className="">
+              <a href={data?.socials.instagram} className="">
                 Instagram
               </a>
-              <a href={data?.socials.li} className="">
+              <a href={data?.socials.linkedIn} className="">
                 LinkedIn
               </a>
               <a href={data?.socials.x} className="">
@@ -37,8 +37,8 @@ export default function Footer() {
               </a>
             </div>
             <div className="flex w-1/2 flex-col md:w-1/2">
-              <a href={data?.privacyPolicy.fileUrl}>Privacy Policy</a>
-              <a href={data?.legal.fileUrl}>Legal</a>
+              <a href={data?.privacyPolicy?.fileUrl || "#"}>Privacy Policy</a>
+              <a href={data?.legal?.fileUrl || "#"}>Legal</a>
             </div>
           </div>
           <div className="flex w-full grow text-balance pt-12 md:w-1/2 md:pt-0">
