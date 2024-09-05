7588769561c2527602a815d3133b8a48c9426411
memellaguno <memellaguno@gmail.com>
2024-09-05 09:36:43 -0600

    Project Dialog is now scrollable

--- components/modules/FeaturedProjects.jsx
+++ components/modules/FeaturedProjects.jsx
@@ -1,8 +1,9 @@
-import { ArrowRight } from "lucide-react";
+import { ArrowRight, Scroll } from "lucide-react";
 import Image from "next/image";
 import { Dialog, DialogContent, DialogTrigger } from "../ui/dialog";
 import { Carousel, CarouselContent, CarouselItem } from "../ui/carousel";
-import { ScrollArea } from "@radix-ui/react-scroll-area";
+import * as DialogRadix from "@radix-ui/react-dialog";
+import { ScrollArea } from "../ui/scroll-area";
 
 export default function FeaturedProjects({ data }) {
   // let dataShape = {
@@ -34,7 +35,7 @@ export default function FeaturedProjects({ data }) {
           return (
             <Dialog key={project._key}>
               <DialogTrigger asChild>
-                <div className="border-1 flex h-[450px] w-full flex-col md:w-1/3">
+                <div className="border-1 flex h-[450px] w-full cursor-pointer flex-col md:w-1/3">
                   <Image
                     src={project.gallery[0].imageUrl}
                     alt={project.gallery[0].alt}
@@ -56,37 +57,70 @@ export default function FeaturedProjects({ data }) {
                   </div>
                 </div>
               </DialogTrigger>
-              <DialogContent className="w-11/12 rounded-sm md:min-w-[800px]">
-                <div className="h-auto md:w-full">
-                  <div className="flex flex-col">
-                    <h2 className="text-xl">{project.title}</h2>
-                    <div className="border-t border-gray-300 pt-1">
-                      <h3 className="text-sm text-gray-500">
-                        {project.caption}
-                      </h3>
+              <DialogRadix.Overlay>
+                <DialogContent className="w-11/12 rounded-sm md:min-w-[800px]">
+                  <ScrollArea className="h-[90vh]">
+                    <div className="h-auto md:w-full md:px-3">
+                      <div className="flex flex-col">
+                        <h2 className="text-xl">{project.title}</h2>
+                        <div className="border-t border-gray-300 pt-1">
+                          <h3 className="text-sm text-gray-500">
+                            {project.caption}
+                          </h3>
+                          <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing
+                            elit. Pellentesque massa elit, vulputate sed tempor
+                            eget, molestie id est. In vel est vitae arcu aliquam
+                            ultrices id at mauris. Proin nunc diam, cursus
+                            elementum odio id, auctor tincidunt ligula. Fusce
+                            venenatis gravida elit, ornare viverra ante viverra
+                            vitae. Aliquam tincidunt pellentesque convallis. Sed
+                            porttitor magna vitae justo varius facilisis. Donec
+                            venenatis malesuada neque, mattis rhoncus quam
+                            porttitor id. Mauris et tortor nisi. Nullam volutpat
+                            nisi libero, sit amet molestie enim porttitor nec.
+                            Praesent ligula quam, molestie vel blandit in,
+                            iaculis non ipsum. Orci varius natoque penatibus et
+                            magnis dis parturient montes, nascetur ridiculus
+                            mus. Ut vitae neque mattis justo porttitor aliquam
+                            vitae vel sapien. Integer a diam egestas, facilisis
+                            urna quis, porttitor diam. Nulla in ex et dui
+                            condimentum facilisis tempus non quam. Cras magna
+                            purus, luctus quis luctus ullamcorper, imperdiet nec
+                            enim. Ut elementum, diam at semper pulvinar, est
+                            felis lobortis odio, sed porttitor lorem erat eu
+                            eros. Fusce gravida facilisis nisl viverra pharetra.
+                            Pellentesque semper mattis tempus. Morbi finibus
+                            justo nec tristique maximus. Etiam sodales erat eu
+                            tellus convallis tempus. Vestibulum rhoncus
+                            venenatis mattis. Nullam a fermentum enim. Donec
+                            vehicula neque in odio rhoncus aliquam.
+                          </p>
+                        </div>
+                      </div>
+                      <Carousel className="pt-4">
+                        <CarouselContent className="-ml-4 pl-0">
+                          {project.gallery.map((image, index) => (
+                            <CarouselItem key={index} className="">
+                              <div className="relative h-96 overflow-hidden rounded-lg bg-yellow-400 md:h-[555px] md:w-auto">
+                                <Image
+                                  src={image.imageUrl}
+                                  alt={image.alt}
+                                  key={image._key}
+                                  width={1200}
+                                  height={1200}
+                                  className="h-full object-cover"
+                                />
+                                {image.alt}
+                              </div>
+                            </CarouselItem>
+                          ))}
+                        </CarouselContent>
+                      </Carousel>
                     </div>
-                  </div>
-                  <Carousel className="pt-4">
-                    <CarouselContent className="-ml-4 pl-0">
-                      {project.gallery.map((image, index) => (
-                        <CarouselItem key={index} className="">
-                          <div className="relative h-96 overflow-hidden rounded-lg bg-yellow-400 md:h-[555px] md:w-auto">
-                            <Image
-                              src={image.imageUrl}
-                              alt={image.alt}
-                              key={image._key}
-                              width={1200}
-                              height={1200}
-                              className="h-full object-cover"
-                            />
-                            {image.alt}
-                          </div>
-                        </CarouselItem>
-                      ))}
-                    </CarouselContent>
-                  </Carousel>
-                </div>
-              </DialogContent>
+                  </ScrollArea>
+                </DialogContent>
+              </DialogRadix.Overlay>
             </Dialog>
           );
         })}
