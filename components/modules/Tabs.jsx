13632c57380bc4ceb73b41f47574b7d9b46b3ed6
memellaguno <memellaguno@gmail.com>
2024-09-19 11:45:29 -0600

    Tabs ready

--- components/modules/Tabs.jsx
+++ components/modules/Tabs.jsx
@@ -39,29 +39,29 @@ export default function Tabs({ data }) {
         orientation="vertical"
         defaultValue={data?.tabs[0]._key}
       >
-        <div className="flex h-full w-full flex-col justify-between md:w-1/2">
-          <div className="flex flex-col pb-10">
+        <section className="flex h-full w-full flex-col justify-between md:w-1/2">
+          <heading className="flex flex-col pb-10">
             <div className="mb-4 flex max-w-fit flex-row items-center justify-center gap-1 pl-1">
               <CircleIcon />
               <p className="text-sm uppercase">{data?.caption}</p>
             </div>
-            <h1 className="text-balance text-left text-6xl leading-none tracking-tight">
+            <h1 className="text-balance text-left text-5xl leading-none tracking-tight md:text-6xl">
               {data?.heading}
             </h1>
-          </div>
-          <div className="font-body flex">
-            <TabsList>
+          </heading>
+          <section className="">
+            <TabsList className="font-body flex flex-col">
               {data?.tabs.map((tab) => {
                 console.log(tab);
                 return (
                   <TabsTrigger
-                    className="group flex w-full flex-col content-start"
+                    className="group flex h-fit w-full flex-col content-start"
                     value={tab._key}
                     key={tab._key}
                   >
                     <Separator className="my-4 border-t group-rdx-state-active:border-black group-rdx-state-inactive:border-gray-400" />
                     <section className="flex h-full w-full flex-row items-center justify-between md:group-rdx-state-active:pb-9">
-                      <h1 className="ml-[-2px] text-left text-5xl tracking-tight group-rdx-state-active:text-black group-rdx-state-inactive:text-neutral-500">
+                      <h1 className="ml-[-2px] text-left text-2xl tracking-tight group-rdx-state-active:text-black group-rdx-state-inactive:text-neutral-500 md:text-5xl">
                         {tab.heading}
                       </h1>
                       <figure className="flex size-10 items-center justify-center rounded-full bg-primary text-white">
@@ -69,21 +69,21 @@ export default function Tabs({ data }) {
                       </figure>
                     </section>
                     <TabsContent value={tab._key} className="flex">
-                      <p className="text-left group-rdx-state-inactive:text-neutral-500">
+                      <p className="pt-4 text-left group-rdx-state-inactive:text-neutral-500">
                         {tab.description}
                       </p>
                     </TabsContent>
                     {/* Mobile */}
                     <TabsContent
                       value={tab._key}
-                      className="flex pb-4 md:hidden"
+                      className="flex group-rdx-state-active:pt-5 md:hidden"
                     >
                       <Image
                         src={tab.image.imageUrl}
                         alt={tab.image.alt}
                         width={1200}
                         height={1200}
-                        className="h-full object-cover"
+                        className="h-full rounded-md object-cover"
                         placeholder="blur"
                         blurDataURL={tab.image.blurDataURL}
                       />
@@ -92,8 +92,8 @@ export default function Tabs({ data }) {
                 );
               })}
             </TabsList>
-          </div>
-        </div>
+          </section>
+        </section>
         <div className="hidden h-full w-3/5 overflow-hidden rounded-lg bg-blue-300 sm:hidden md:flex">
           {data?.tabs.map((tab) => {
             return (
