702c893370cb1b5b637abffd880c5511597a9749
memellaguno <memellaguno@gmail.com>
2024-09-23 10:55:42 -0600

    forcing refresh

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,21 +1,78 @@
-import { createClient } from "next-sanity";
+// ./src/sanity/lib/client.ts
+
+import "server-only";
+
+import { createClient, QueryOptions, type QueryParams } from "next-sanity";
+import { draftMode } from "next/headers";
 
 import { apiVersion, dataset, projectId } from "../env";
+import { token } from "./token";
 
 export const client = createClient({
   projectId,
   dataset,
-  apiVersion,
-  useCdn: process.env.NODE_ENV === "development" ? true : false, // Set to false if statically generating pages, using ISR or tag-based revalidation
+  apiVersion, // https://www.sanity.io/docs/api-versioning
+  useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
+  stega: {
+    enabled: process.env.NEXT_PUBLIC_VERCEL_ENV === "preview",
+    studioUrl: "/studio",
+  },
 });
 
-// revalidate: process.env.NODE_ENV === "development" ? 30 : 3600,
+export async function sanityFetch<QueryResponse>({
+  query,
+  params = {},
+  revalidate = 60, // default revalidation time in seconds
+  tags = [],
+}: {
+  query: string;
+  params?: QueryParams;
+  revalidate?: number | false;
+  tags?: string[];
+}) {
+  const isDraftMode = draftMode().isEnabled;
 
-export async function sanityFetch({ query, params = {}, tags }) {
-  return client.fetch(query, params, {
-    cache: "force-cache",
+  if (isDraftMode && !token) {
+    throw new Error("Missing environment variable SANITY_API_READ_TOKEN");
+  }
+
+  let queryOptions: QueryOptions = {};
+  let maybeRevalidate = revalidate;
+
+  if (isDraftMode) {
+    queryOptions.token = token;
+    queryOptions.perspective = "previewDrafts";
+    queryOptions.stega = true;
+    console.log("Draft mode is enabled:", isDraftMode);
+
+    maybeRevalidate = 0; // Do not cache in Draft Mode
+  } else if (tags.length) {
+    maybeRevalidate = false; // Cache indefinitely if tags supplied
+  }
+
+  return client.fetch<QueryResponse>(query, params, {
+    ...queryOptions,
     next: {
+      revalidate: maybeRevalidate,
       tags,
     },
   });
 }
+
+//prev Config
+// export const client = createClient({
+//   projectId,
+//   dataset,
+//   apiVersion,
+//   useCdn: process.env.NODE_ENV === "development" ? true : false, // Set to false if statically generating pages, using ISR or tag-based revalidation
+// });
+
+// // revalidate: process.env.NODE_ENV === "development" ? 30 : 3600,
+
+// export async function sanityFetch({ query, params = {}, tags }) {
+//   return client.fetch(query, params, {
+//     cache: "force-cache",
+//     next: {
+//       tags,
+//     },
+//   });
